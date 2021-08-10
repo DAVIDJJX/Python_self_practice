5b1ab28b4a418cd765bf18ec1a0b003c6bff929c
DAVIDJJX <davidnba123@gmail.com>
2021-08-10 14:46:13 +0800

    99 Multiplication Table

--- 20210806/99recursion.py
+++ 20210806/99recursion.py
@@ -1,15 +1,15 @@
-def func(a,b):
+## 99乘法表
+def func(a:int,b:int):
     c=int(a)*int(b)
     print(a,'*',b,'=',c)
     if a==1 and b==1:
-        c==1
-        retun c
+        return c
     elif a!=1 and b==1:
-        print(end='/n')
-        func(a-1,b)
+        print(end='\n')
+        func(a-1,9)
     else:
         func(a,b-1)
-    
+        
 func(9,9)
 
 
